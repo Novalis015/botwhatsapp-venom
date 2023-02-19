77e438df5d6613e9b2a0032b3b18fe36c7644c6a
Novalis015 <arthur.novaes95@gmail.com>
2023-02-18 22:28:37 -0300

    Revert "fix a bug on start venom"
    This reverts commit 1629c8651bef46edf91de78b9999086210833a99.

--- src/server.js
+++ src/server.js
@@ -1,34 +1,37 @@
 import { create } from 'venom-bot';
 import { stages, getStage } from './stages.js';
 
-create({
-    session: 'store',
-    multidevice: true,
-    headless: false,
-  })
+create({ session: 'delicias-neide' })
   .then((client) => start(client))
   .catch((error) => {
-    console.error(error);
-    process.exit(1);
+    process.exit(error);
   });
 
+async function start(client) {
+  await client.onMessage(async (message) => {
+    const isValidNumber = await client.checkNumberStatus(message.from);
+    if (!message.isGroupMsg && isValidNumber) {
+      try {
+        const currentStage = getStage({ from: message.from });
+        console.log(currentStage);
 
-function start(client) {
-    client.onMessage((message) => {
-      if (!message.isGroupMsg) {
-          const currentStage = getStage({ from: message.from });
+        const messageResponse = stages[currentStage].stage.exec({
+          from: message.from,
+          message: message.body,
+          client,
+        });
 
-          const messageResponse = stages[currentStage].stage.exec({
-            from: message.from,
-            message: message.body,
-            client,
-          });
-  
-          if (messageResponse) {
-            client.sendText(message.from, messageResponse).then(() => {
-              console.log('Message sent.');
-            }).catch(error => console.error('Error when sending message', error));
-          }
+        if (messageResponse) {
+          await client.sendText(message.from, messageResponse);
+        }
+      } catch (error) {
+        console.log(error);
+        client.close();
       }
-    });
-};
+    }
+  });
+
+  process.on('SIGINT', function () {
+    client.close();
+  });
+}
